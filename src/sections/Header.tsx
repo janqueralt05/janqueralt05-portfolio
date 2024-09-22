43310a3a5ed50d61383a6989fffb36f788e352ab
janqueralt05 <jan@alumnes.ilerna.com>
2024-09-23 01:05:18 +0200

    Vercel deploy

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,11 +1,12 @@
 export const Header = () => {
   return (
-    <div className="flex justify-center items-cente relative top-3">
+    <div className="flex justify-center items-cente fixed top-3 w-full">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         <a href="#" className="nav-item">Inicio</a>
         <a href="#" className="nav-item">Proyectos</a>
         <a href="#" className="nav-item">Información</a>
-        <a href="#" className="nav-item bg-white text-gray-900">Contáctame</a>
+        <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contáctame</a>
+        <a href="https://github.com/janqueralt05" target="_blank" className="nav-item">Github</a>
       </nav>
     </div>
   );
