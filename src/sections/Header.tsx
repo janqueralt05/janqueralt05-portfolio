fbcdd5b061ff7c0521da4996010a53768cb936f5
janqueralt05 <jan@alumnes.ilerna.com>
2024-09-23 00:12:14 +0200

    hi

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,11 +1,11 @@
 export const Header = () => {
   return (
-    <div>
-      <nav>
-        <a href="#">Inicio</a>
-        <a href="#">Proyectos</a>
-        <a href="#">Información</a>
-        <a href="#">Contáctame</a>
+    <div className="flex justify-center items-cente relative top-3">
+      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+        <a href="#" className="nav-item">Inicio</a>
+        <a href="#" className="nav-item">Proyectos</a>
+        <a href="#" className="nav-item">Información</a>
+        <a href="#" className="nav-item bg-white text-gray-900">Contáctame</a>
       </nav>
     </div>
   );
