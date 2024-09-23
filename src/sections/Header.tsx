8480b9d0b1d726b9d7a02b78f5720ea25bd82bbc
janqueralt05 <jan@alumnes.ilerna.com>
2024-09-23 04:53:35 +0200

    1.0

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,11 +2,11 @@ export const Header = () => {
   return (
     <div className="flex justify-center items-cente fixed top-3 w-full">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-        <a href="#" className="nav-item">Inicio</a>
-        <a href="#" className="nav-item">Proyectos</a>
-        <a href="#" className="nav-item">Información</a>
-        <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contáctame</a>
-        <a href="#" target="_blank" className="nav-item">Changelog</a>
+        <a href="#" className="nav-item">🏠 Inicio</a>
+        <a href="#" className="nav-item">💻 Proyectos</a>
+        <a href="#" className="nav-item">📚 Información</a>
+        <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">👋 Contáctame</a>
+        <a href="#" target="_blank" className="nav-item">🔄 Changelog</a>
       </nav>
     </div>
   );
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,10 +3,15 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
 import memojiImage from "@/assets/images/memoji-computer.png";
+import grainImage from "@/assets/images/grain.jpg";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md:py-48 lg:py-60 relative z-0">
+      <div className="absolute inset-0 -z-30 opacity-5" style={{
+        backgroundImage: `url(${grainImage.src})`,
+      }}></div>
+      <div className="absolute inset-0 size-[700px] border-2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full border-emerald-300/5 shadow-[0_0_80px_inset] shadow-emerald-300/5"></div>
       <Analytics />
       <SpeedInsights />
       <div className="container">
@@ -14,12 +19,14 @@ export const HeroSection = () => {
         <Image src={memojiImage} className="size-[100px]" alt="person" />
       <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
           <div className="bg-green-500 rounded-full size-2.5"></div>
-          <div className="text-sm font-medium">Disponible</div>
+          <div className="text-sm font-medium">Abierto a tus ideas ✨</div>
         </div>
       </div>
-      <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">Mejorando dia a dia</h1>
-      <p className="mt-4 text-center text-white/60">Soy desarrollador web full stack, me gusta crear aplicaciones web que sean intuitivas y funcionales, me gusta aprender cosas nuevas y trabajar en equipo.</p>
-      <div className="flex flex-col items-center mt-8 gap-4">
+      <div className="max-w-lg mx-auto">
+        <h1 className="font-serif text-3xl text-5xl text-center mt-8 tracking-wide">Construyendo el futuro día tras día...</h1>
+        <p className="mt-4 text-center text-white/60 md:text-lg">Soy desarrollador web full stack, me gusta crear aplicaciones web que sean intuitivas y funcionales, me gusta aprender cosas nuevas y trabajar en equipo.</p>
+      </div>
+      <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
         <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
           <span className="font-semibold">Explora mi trabajo</span>
           <ArrowDown className="size-4" />
